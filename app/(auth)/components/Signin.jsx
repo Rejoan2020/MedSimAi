d6a0bf946d7ae5b617708b82feca85bbe6034b7b
Rejoan2020 <rej.ash11@gmail.com>
2026-08-17 15:04:21 +0600

    connected to mongodb

--- app/(auth)/components/Signin.jsx
+++ app/(auth)/components/Signin.jsx
@@ -1,11 +1,14 @@
+'use client'
 import { Key, Lock, Mail } from 'lucide-react'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Signin() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     return (
-        <div className='grid xl:grid-cols-2 p-8 pt-32 w-full'>
-            <div className='flex flex-col justify-center items-center gap-16'>
+        <div className='grid xl:grid-cols-2 items-center w-full h-full'>
+            <div className='flex flex-col w-full items-center gap-16'>
                 <Image height={32} width={162} alt='logo' src={'/logo/medsimailogo.png'}
                     className='h-8 w-40' />
                 <div>
@@ -17,7 +20,12 @@ export default function Signin() {
                         <div>Email address</div>
                         <div className='border flex items-center w-full px-2 border-gray-200 rounded-lg'>
                             <Mail />
-                            <input className='h-12 outline-none px-4 w-full'>
+                            <input
+                                name='email'
+                                value={email}
+                                type='email'
+                                onChange={(e) => setEmail(e.target.value)}
+                                className='h-12 outline-none px-4 w-full'>
                             </input>
                         </div>
                     </div>
@@ -25,18 +33,27 @@ export default function Signin() {
                         <div>Password</div>
                         <div className='border flex items-center w-full px-2 border-gray-200 rounded-lg'>
                             <Lock />
-                            <input className='h-12 outline-none px-4 w-full'>
+                            <input
+                                name='password'
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                type='password'
+                                className='h-12 outline-none px-4 w-full'>
                             </input>
                         </div>
                     </div>
-                    <div className='border p-2 flex justify-center items-center bg-light-blue text-white rounded-md'>Sign in</div>
+                    <button
+                        type='submit'
+                        className='border p-2 flex justify-center items-center bg-light-blue text-white rounded-md cursor-pointer'>
+                        Sign in
+                    </button>
                     <div className='text-Secondary-Text'>Don't have an account? contact institution</div>
                 </div>
 
             </div>
 
-            <div className='w-full hidden xl:block'>
-                <Image height={600} width={600} alt='image' src={'/sidelogin.svg'} className='min-full' />
+            <div className='w-full h-full hidden xl:block '>
+                <Image height={800} width={600} alt='image' src={'/sidelogin.svg'} className='h-full' />
             </div>
         </div>
     )
